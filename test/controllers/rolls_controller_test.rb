7a01a8d1104b05778811dfaa7eaa216503411cec
Mark Windholtz <mark@agiledna.com>
2014-12-25 15:20:42 -0500

    tweak

--- test/controllers/rolls_controller_test.rb
+++ test/controllers/rolls_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class RollsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
+
+  def the_truth
 	  Die.expects(:roll_spec).returns(18)
     xhr :get, :new, :dice => 'd6'
     assert_equal "<span id='roll-result'>18</span>", @response.body 
